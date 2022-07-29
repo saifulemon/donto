4a9fa54a799d91b4b006791c6bce329448ece7e9
saifulemon <463saiful0@gmail.com>
2022-07-29 18:01:18 +0600

    Banner section completed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './Banner.css';
+import "@fontsource/josefin-sans";
+import womanbrush from '../../../Images/woman-brush.c4158ac5.png';
+import doctorfinding from '../../../Images/doctorfinding.c2532ac3.png';
+import heroTeeth from '../../../Images/hero-theeth.54c2c4e9.png';
 
 const Banner = () => {
     return (
         <div>
-            <h3>This is Banner</h3>
+            <section className="single-hero-slide text-white">
+                <Container>
+                    <Row>
+                        <Col md={6} sm={12}>
+
+                            <div className="hero-slide-left justify-content-end align-items-center text-start">
+                                <h2>Better Life Through</h2>
+                                <h1>Better Dentistry</h1>
+                                <p>Join us to a fun and friendly dental environment. Our professionals are working so hard to see smile on your face that you deserve! We are dedicated about our duties.</p>
+                                <button className="theme-btn btn-fill">Appoinment</button>
+                                <button className='theme-btn bth-blank'>Learn More</button>
+                            </div>
+                        </Col>
+                        <Col md={6} sm={12}>
+                            <div className="hero-slide-right text-start">
+                                <div className="animate-img">
+                                    <img src={womanbrush} alt="" className="img-fluid aimg1" />
+                                    <img src={doctorfinding} alt="" className="img-fluid aimg2" />
+                                </div>
+                                <img src={heroTeeth} alt="" className="img-fluid"/>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </div>
     );
 };
