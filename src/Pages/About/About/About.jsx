e5c6632e66623698e5de2b08e73f05ae182d4949
saifulemon <463saiful0@gmail.com>
2022-08-25 22:23:50 +0600

    about page successfully completed

--- src/Pages/About/About/About.jsx
+++ src/Pages/About/About/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AboutService from '../../../components/AboutService/AboutService';
+import LatestBlog from '../../../components/LatestBlog/LatestBlog';
+import Appoinment from '../../Home/Appoinment/Appoinment';
 import Content from '../AboutContent/Content';
 import Achivement from '../Achivement/Achivement';
 import Banner from '../Banner/Banner';
@@ -13,6 +15,8 @@ const About = () => {
           <AboutService />
           <Dentist />
           <Achivement />
+          <LatestBlog />
+          <Appoinment />
         </>
     );
 };
